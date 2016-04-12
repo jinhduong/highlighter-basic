311866ec60968134f6e8fcd122ebee75631ce957
jinhduong <0961020@gmail.com>
2016-04-13 01:26:45 +0800

    Update add description for element

--- main.js
+++ main.js
@@ -9,6 +9,19 @@ var
         },
         d: {
             preNodeId: 0
+        },
+        html: {
+            popup: '<div class="ce-popup"> \
+                <div class="ce-content"> \
+                    <h3> Add Description </h3> \
+                    <input type="text" /> <button>Add</button> \
+                </div> \
+            </div>',
+            popup_mini: '<div class="ce-popup-mini"> \
+                <div class="ce-content"> \
+                    <span></span> \
+                </div> \
+            </div>'
         }
     };
 
@@ -17,18 +30,9 @@ if (tree != null) {
     thisPage && reloadPage(thisPage);
 }
 
-function saveSelectedText(selectObj) {
-    var info = {
-        text: selectObj.toString().trim(),
-        anchor: selectObj.anchorNode,
-        inData: selectObj.anchorNode.data.trim(),
-        parent: $(selectObj.anchorNode.parentElement),
-        sender: selectObj,
-        place: place
-    };
-
+function saveSelectedText(selectObj, decs, info) {
     var guid = shortGuid();
-    var newHtml = processStr(info.parent.html(), info.text, info.sender.anchorOffset, guid);
+    var newHtml = processStr(info.parent.html(), info.text, info.sender.anchorOffset, guid, decs);
     info.parent.html(newHtml);
 
     if (tree == null || tree[info.place] == undefined)
@@ -40,13 +44,32 @@ function saveSelectedText(selectObj) {
         text: info.text,
         guid: guid
     });
-    
     updateStore();
+    injection();
 }
 
+$('body').append(store$.html.popup);
+$('body').append(store$.html.popup_mini);
+
 window.addEventListener("keydown", function (e) {
-    if (e.keyCode == 66 && e.ctrlKey)
-        saveSelectedText(selection());
+    if (e.keyCode == 66 && e.ctrlKey) {
+        var selectObj = selection();
+        var info = {
+            text: selectObj.toString().trim(),
+            anchor: selectObj.anchorNode,
+            inData: selectObj.anchorNode.data.trim(),
+            parent: $(selectObj.anchorNode.parentElement),
+            sender: selectObj,
+            place: place
+        };
+        var pos = $(selectObj.anchorNode.parentElement).offset().top;
+        kDescription({
+            pos: pos,
+            clickAdd: function (decs) {
+                saveSelectedText(selectObj, decs, info);
+            }
+        })
+    }
     if (e.keyCode == 78 && e.shiftKey)
         nextHighlight();
 });
@@ -56,8 +79,10 @@ $(document).on('click', '.chrome-extension-highlight', function () {
     removeElement.call(null, $(this));
 })
 
-function processStr(fullString, text, from, guid) {
-    var spanHtml = $('<span>').text(text).attr('guid', guid).addClass(store$.class.YELLOW)[0].outerHTML;
+injection();
+
+function processStr(fullString, text, from, guid, desc) {
+    var spanHtml = $('<span>').text(text).attr('guid', guid).attr('desc', desc).addClass(store$.class.YELLOW)[0].outerHTML;
     var first = fullString.substr(0, from),
         second = fullString.substr(from);
     second = second.replace(text, spanHtml);
@@ -97,9 +122,36 @@ function removeElement(element) {
             thisPage.splice(index, 1);
     });
     updateStore();
-    
+
 }
 
-function updateStore(){
+function updateStore() {
     localStorage.setItem('hl', JSON.stringify(tree));
+}
+
+function kDescription(settings) {
+    var config = {
+        pos: settings.pos,
+        clickAdd: settings.clickAdd
+    };
+
+    $('.ce-popup').css('top', config.pos).show();
+    $('.ce-popup').find('button').click(function (e) {
+        var desc = $('.ce-popup').find('input').val();
+        config.clickAdd(desc);
+        $('.ce-popup').hide();
+    });
+}
+
+function injection() {
+    $('.chrome-extension-highlight').mouseover(function (e) {
+        var pos = $(this).offset().top + 20;
+        var posLeft = $(this).offset().left + 20;
+        $('.ce-popup-mini').css('top', pos).css('left', posLeft).find('span').text($(this).attr('desc'));
+        $('.ce-popup-mini').show();
+    });
+
+    $('.chrome-extension-highlight').mouseleave(function (e) {
+        $('.ce-popup-mini').hide();
+    })
 }
